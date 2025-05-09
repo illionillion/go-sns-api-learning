fe3d289aa807d290fb51aad78828b9cee5a0fdae
yuto ichinose <yuto.0526.sano@gmail.com>
2025-05-09 09:46:21 +0900

    feat: ユーザーモデル修正

--- models/user.go
+++ models/user.go
@@ -1,22 +1,56 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 /*
 ユーザーのデータ
 */
 type User struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Email     string    `json:"email" gorm:"unique"`
-	Password  string    `json:"password"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID         uint      `json:"id" gorm:"primaryKey"`
+	Email      string    `json:"email" gorm:"unique"`
+	Password   string    `json:"password"`
+	AvatarURL  string    `json:"avatar_url"`
+	HeaderURL  string    `json:"header_url"`
+	Bio        string    `json:"bio"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+// Check if given password is match to account's password
+func (u *User) CheckPassword(pass string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(pass)) == nil
+}
+
+// Hash password and set it to account object
+func (u *User) SetPassword(pass string) error {
+	passwordHash, err := generatePasswordHash(pass)
+	if err != nil {
+		return fmt.Errorf("generate error: %w", err)
+	}
+	u.Password = passwordHash
+	return nil
+}
+
+func generatePasswordHash(pass string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("hashing password failed: %w", err)
+	}
+	return string(hash), nil
 }
 
 /*
 APIで返すユーザーのデータ
 */
 type UserResponse struct {
-	ID    uint   `json:"id" gorm:"primaryKey"`
-	Email string `json:"email" gorm:"unique"`
+	ID        uint   `json:"id"`
+	Email     string `json:"email"`
+	AvatarURL string `json:"avatar_url"`
+	HeaderURL string `json:"header_url"`
+	Bio       string `json:"bio"`
 }
